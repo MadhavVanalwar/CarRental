52e67e07c0f0fc828f3c62f0085473979b6cd6a0
MadhavVanalwar <madhavvanalwar143@gmail.com>
2023-04-07 10:25:33 +0530

    Add one h1 Tag

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import './App.css';
 
 function App() {
   return (
+    <>
     <h1>this is App component</h1>
+    <h1>Hello,Dinesh Sir I am Changed code please accept my request </h1>
+    </>
   );
 }
 
