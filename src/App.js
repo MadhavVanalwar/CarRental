81eb01aca7a253cdf9f0b4531721f5c48271de72
MadhavVanalwar <madhavvanalwar143@gmail.com>
2023-04-07 12:26:30 +0530

    added to version-2

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <h1>this is App component</h1>
     <h1>Hello,Dinesh Sir I am Changed code please accept my request </h1>
     <h1>This is the created new branch and that name is version-1</h1>
+    <h1>this is version -2</h1>
     </>
   );
 }
